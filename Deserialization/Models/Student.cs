227cb41790820dae6dcc4b107db9bbc0b0f83cba
Pradnya Shubham Jekate <pradnyajekate2000@gmail.com>
2026-07-25 12:19:25 +0530

    Deserialization

---
+++ Deserialization/Models/Student.cs
@@ -0,0 +1,7 @@
+namespace Deserialization.Models;
+public class Student
+{
+    public int Id{get; set;}
+    public string?  Name{get; set;}
+    public string? Course{get; set;}
+}
---
+++ Deserialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Deserialization.Models;
+class Program
+{
+    static void Main()
+    {
+        // Read the JSON file
+        string json=File.ReadAllText("student.json");
+        // Deserialize the JSON into a Student object
+        Student? student=JsonSerializer.Deserialize<Student>(json);
+        // Display the student information
+        if(student!=null)
+        {
+           Console.WriteLine("Deserialization successful!");
+           Console.WriteLine("Id:"+student.Id);
+           Console.WriteLine("Name:"+student.Name);
+           Console.WriteLine("Course:"+student.Course);
+        }
+        else
+        {
+            Console.WriteLine("Failed to deserialize the JSON");
+        }
+    }
+}
