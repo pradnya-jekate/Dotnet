73e3b78c2a182feaab6f631cbc847efb8cce37f9
Pradnya Shubham Jekate <pradnyajekate2000@gmail.com>
2026-07-25 11:50:12 +0530

    Serialization

---
+++ Serialization/Models/Student.cs
@@ -0,0 +1,10 @@
+using System;
+using System.IO;
+using System.Text.Json;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Course{get; set;}
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+class Program
+{
+    static void Main()
+    {
+        Student student=new Student()
+        {
+            Id=1,
+            Name="Pradnya",
+            Course="Dotnet"
+
+        };
+
+
+        string json=JsonSerializer.Serialize(student);
+        File.WriteAllText("student.json",json);
+        Console.WriteLine("Serialization successful");
+        Console.WriteLine(json);
+
+    }
+
+}
+
+//Serialization is the process of converting an object into a format such as JSON, XML, or 
+// Binary so that it can be saved, stored, or transferred.
+
+
+//1.Web App) When a client sends or receives data, objects are converted to JSON.
